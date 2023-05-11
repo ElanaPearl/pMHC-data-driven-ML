6ffae718e58cac9590f92ee5789f6ce1e8d3856c
Elana Simon <elanasimon95@gmail.com>
2023-05-11 15:13:22 -0700

    reformat

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,4 +1,3 @@
-import os
 import tarfile
 import tempfile
 import urllib.request
@@ -7,8 +6,8 @@ from pathlib import Path
 import pandas as pd
 
 
-def download_data(download_url: str, download_dir: Path = Path("../data")) -> Path:
-    """ Downloads data, extracts it, and returns the path to the extracted folder"""
+def download_data(download_url: str, download_dir: Path) -> Path:
+    """Downloads data, extracts it, and returns the path to the extracted folder"""
     tmp_download_fn = tempfile.NamedTemporaryFile(suffix=".tar.gz").name
     urllib.request.urlretrieve(download_url, tmp_download_fn)
     with tarfile.open(tmp_download_fn, "r:gz") as tar:
@@ -17,18 +16,28 @@ def download_data(download_url: str, download_dir: Path = Path("../data")) -> Pa
     return download_dir / extracted_dir_name
 
 
-def reformat_downloaded_data(netmhc_data_folder: Path, output_path: Path = Path("../data/IEDB_regression_data.csv")):
-    """ Reformats BA data from NetMHCpan download into one CSV"""
+def reformat_downloaded_data(netmhc_data_folder: Path, output_path: Path):
+    """Reformats BA data from NetMHCpan download into one CSV"""
 
     # Load in MHC to pseudo sequence mapping
-    mhc_to_pseudo_seq = pd.read_csv(netmhc_data_folder / "MHC_pseudo.dat",
-                                    delim_whitespace=True,
-                                    names=["mhc", "pseudo_seq"]).set_index("mhc")["pseudo_seq"].to_dict()
+    mhc_to_pseudo_seq = (
+        pd.read_csv(
+            netmhc_data_folder / "MHC_pseudo.dat",
+            delim_whitespace=True,
+            names=["mhc", "pseudo_seq"],
+        )
+        .set_index("mhc")["pseudo_seq"]
+        .to_dict()
+    )
 
     # Read in BA data splits
     all_splits = []
     for cv_split in range(5):
-        data = pd.read_csv(netmhc_data_folder / f'c00{cv_split}_ba', delim_whitespace=True, names=["peptide", "affinity", "mhc_name"])
+        data = pd.read_csv(
+            netmhc_data_folder / f"c00{cv_split}_ba",
+            delim_whitespace=True,
+            names=["peptide", "affinity", "mhc_name"],
+        )
         data["cv_split"] = cv_split
         all_splits.append(data)
     data = pd.concat(all_splits, axis=0)
@@ -48,7 +57,8 @@ if __name__ == "__main__":
 
     print("Downloading data...")
     netmhc_data_folder = download_data(download_url, download_dir=data_dir)
-    print("netmhc_data_folder:", netmhc_data_folder)
+
     print("Reformatting data...")
     reformat_downloaded_data(netmhc_data_folder, output_path=output_path)
+
     print("Done!")
