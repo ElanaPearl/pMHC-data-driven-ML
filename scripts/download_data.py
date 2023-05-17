321497ec1dab39af09099a6b06b330a019582429
Elana Simon <elanasimon95@gmail.com>
2023-05-16 23:09:32 -0700

    added in el data download

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -16,49 +16,112 @@ def download_data(download_url: str, download_dir: Path) -> Path:
     return download_dir / extracted_dir_name
 
 
-def reformat_downloaded_data(netmhc_data_folder: Path, output_path: Path):
+def reformat_downloaded_ba_data(
+    netmhc_data_folder: Path, output_path: Path, mhc_to_pseudo_seq: dict
+):
     """Reformats BA data from NetMHCpan download into one CSV"""
 
-    # Load in MHC to pseudo sequence mapping
-    mhc_to_pseudo_seq = (
-        pd.read_csv(
-            netmhc_data_folder / "MHC_pseudo.dat",
-            delim_whitespace=True,
-            names=["mhc", "pseudo_seq"],
-        )
-        .set_index("mhc")["pseudo_seq"]
-        .to_dict()
-    )
-
     # Read in BA data splits
-    all_splits = []
+    all_ba_splits = []
     for cv_split in range(5):
-        data = pd.read_csv(
+        ba_data = pd.read_csv(
             netmhc_data_folder / f"c00{cv_split}_ba",
             delim_whitespace=True,
             names=["peptide", "affinity", "mhc_name"],
         )
-        data["cv_split"] = cv_split
-        all_splits.append(data)
-    data = pd.concat(all_splits, axis=0)
+        ba_data["cv_split"] = cv_split
+        all_ba_splits.append(ba_data)
+    ba_data = pd.concat(all_ba_splits, axis=0)
 
     # Add in pseudo sequence to data
-    data["mhc_psuedo_seq"] = data["mhc_name"].map(mhc_to_pseudo_seq)
+    ba_data["mhc_psuedo_seq"] = ba_data["mhc_name"].map(mhc_to_pseudo_seq)
 
     # Reorder columns and save
-    data = data[["peptide", "mhc_psuedo_seq", "affinity", "mhc_name", "cv_split"]]
-    data.to_csv(output_path, index=False)
+    ba_data = ba_data[["peptide", "mhc_psuedo_seq", "affinity", "mhc_name", "cv_split"]]
+    ba_data.to_csv(output_path, index=False)
+
+
+def reformat_downloaded_el_data(
+    netmhc_data_folder: Path, output_path: Path, mhc_to_pseudo_seq: dict
+):
+    """Reformats el data from NetMHCpan download into one CSV"""
+    # Read in BA data splits
+    all_el_splits = []
+    for cv_split in range(5):
+        el_data = pd.read_csv(
+            netmhc_data_folder / f"c00{cv_split}_el",
+            delim_whitespace=True,
+            names=["peptide", "presented", "cell_line"],
+        )
+        el_data["cv_split"] = cv_split
+        all_el_splits.append(el_data)
+    el_data = pd.concat(all_el_splits, axis=0)
+
+    # Convert cell line to list of posisble alleles
+    cell_line_to_allele = (
+        pd.read_csv(
+            netmhc_data_folder / "allelelist",
+            delim_whitespace=True,
+            names=["cell_line", "mhc_name"],
+        )
+        .set_index("cell_line")["mhc_name"]
+        .to_dict()
+    )
+    el_data["allele"] = el_data["cell_line"].map(cell_line_to_allele)
+
+    # Convert allele list to pseudo sequence list
+    def allele_list_to_pseudo_seq_list(allele_list):
+        return ",".join(
+            [mhc_to_pseudo_seq[allele] for allele in allele_list.split(",")]
+        )
+
+    el_data["mhc_pseudo_seq"] = el_data["allele"].map(allele_list_to_pseudo_seq_list)
+    el_data["n_possible_alleles"] = el_data["allele"].apply(lambda x: len(x.split(",")))
+
+    # Reorder columns and save
+    el_data = el_data[
+        [
+            "peptide",
+            "mhc_pseudo_seq",
+            "presented",
+            "cell_line",
+            "allele",
+            "n_possible_alleles",
+            "cv_split",
+        ]
+    ]
+    el_data.to_csv(output_path, index=False)
+
+
+def map_mhc_to_pseudo_seq(MHC_pseudo_file: Path):
+    """Create a dictionary mapping MHC name to pseudo sequence"""
+    return (
+        pd.read_csv(MHC_pseudo_file, delim_whitespace=True, names=["mhc", "pseudo_seq"])
+        .set_index("mhc")["pseudo_seq"]
+        .to_dict()
+    )
 
 
 if __name__ == "__main__":
     download_url = "https://services.healthtech.dtu.dk/suppl/immunology/NAR_NetMHCpan_NetMHCIIpan/NetMHCpan_train.tar.gz"
     data_dir = Path("../data")
-    output_path = data_dir / "IEDB_regression_data.csv"
 
     print("Downloading data...")
-    netmhc_data_folder = download_data(download_url, download_dir=data_dir)
+    netmhc_data_folder = download_data(download_url=download_url, download_dir=data_dir)
+    mhc_to_pseudo_seq = map_mhc_to_pseudo_seq(netmhc_data_folder / "MHC_pseudo.dat")
+
+    print("Reformatting el data...")
+    reformat_downloaded_el_data(
+        netmhc_data_folder,
+        output_path=data_dir / "IEDB_classification_data.csv",
+        mhc_to_pseudo_seq=mhc_to_pseudo_seq,
+    )
 
     print("Reformatting data...")
-    reformat_downloaded_data(netmhc_data_folder, output_path=output_path)
+    reformat_downloaded_ba_data(
+        netmhc_data_folder,
+        output_path=data_dir / "IEDB_regression_data.csv",
+        mhc_to_pseudo_seq=mhc_to_pseudo_seq,
+    )
 
     print("Done!")
